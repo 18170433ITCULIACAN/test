7d8d5367c2acf8bc19f588b1902354e6bd026e48
Daniel Eduardo Rios Quintero <18170433@itculiacan.edu.mx>
2024-03-19 14:33:13 -0700

    component, create, edit, index, view and mysql cnx

--- myTest/src/app/app.routes.ts
+++ myTest/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+  
+import { IndexComponent } from './post/index/index.component';
+import { ViewComponent } from './post/view/view.component';
+import { CreateComponent } from './post/create/create.component';
+import { EditComponent } from './post/edit/edit.component';
+import { BienvenidaComponent } from './post/bienvenida/bienvenida.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', redirectTo: 'post/create', pathMatch: 'full'},
+      { path: 'post/index', component: IndexComponent },
+      { path: 'post/:postId/view', component: ViewComponent },
+      { path: 'post/create', component: CreateComponent },
+      { path: 'post/:postId/edit', component: EditComponent }, 
+      { path: 'bienvenida', component: BienvenidaComponent},
+
+  ];
---
+++ myTest/src/app/post/post.ts
@@ -0,0 +1,11 @@
+export interface Post {
+    id: number;
+    nombreCompleto: string
+    nombreEmpresa: string
+    correoElectrónico: string
+    telefono: number
+    categoria: string
+    mensaje: string
+  }
+  
+  
